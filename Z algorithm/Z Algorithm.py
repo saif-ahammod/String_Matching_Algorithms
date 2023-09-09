1f857535667f7be5f175fc5d250491f4dbaa1dfa
Md. Saif Ahammod Khan <saif.ahammod@gmail.com>
2023-09-10 00:09:27 +0600

    Z algorithm with 100 % accuricy

--- Z algorithm/Z Algorithm.py
+++ Z algorithm/Z Algorithm.py
@@ -3,7 +3,6 @@ def calculateZ(input):
     left = 0
     right = 0
     z = [0]*(len(input))
-
     for k in range(1, len(input)):
         # Check if the k is outside the z box
         if k > right:
@@ -32,29 +31,22 @@ def calculateZ(input):
                 z[k] = right - left
                 right = right - 1
     return z
-
 def findMatchPattern(text,pattern):
     concatedText =  pattern+ "$" +text
-
     index = []
     #Calculate the z index
     z = calculateZ(concatedText)
-
     # Find the index of matched pattern in text
     for i in range(0,len(z)):
         if len(pattern) == z[i]:
-            index.append(i-len(pattern)+1)
-
+            index.append(i-len(pattern)-1)
     return index
 
-
-
 def main():
-    text = "xabeabzabc"
-    pattern = "abc"
+    text = input("Please input a text")
+    pattern = input("Please input a pattern")
     matchIndex = findMatchPattern(text,pattern)
-    print(matchIndex)
-
+    print(f"Pattern match found at index {matchIndex}")
 
 if __name__ == "__main__":
     main()
