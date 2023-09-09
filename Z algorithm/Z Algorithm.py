05f985bca7b7dbf7968560e1e5a18c93ccae99d8
Md. Saif Ahammod Khan <saif.ahammod@gmail.com>
2023-09-09 23:50:13 +0600

    Z algorithm added with bug

---
+++ Z algorithm/Z Algorithm.py
@@ -0,0 +1,60 @@
+def calculateZ(input):
+    #initializing z array and values of left and right of z box
+    left = 0
+    right = 0
+    z = [0]*(len(input))
+
+    for k in range(1, len(input)):
+        # Check if the k is outside the z box
+        if k > right:
+            left = right = k
+            while right < len(input):
+                if input[right] == input[right - left]:
+                    right = right + 1
+                else:
+                    break
+            z[k] = right-left
+            right = right -1
+        #if the value of k is inside z box
+        else:
+            #then if the value do not touch the right side of the z box
+            k1 = k-left
+            if(z[k1]<right-k-1):
+                z[k] = z[k1]
+            #if the value do touch the right side of the z box
+            else:
+                left = k
+                while right < len(input):
+                    if input[right] == input[right - left]:
+                        right = right + 1
+                    else:
+                        break
+                z[k] = right - left
+                right = right - 1
+    return z
+
+def findMatchPattern(text,pattern):
+    concatedText =  pattern+ "$" +text
+
+    index = []
+    #Calculate the z index
+    z = calculateZ(concatedText)
+
+    # Find the index of matched pattern in text
+    for i in range(0,len(z)):
+        if len(pattern) == z[i]:
+            index.append(i-len(pattern)+1)
+
+    return index
+
+
+
+def main():
+    text = "xabeabzabc"
+    pattern = "abc"
+    matchIndex = findMatchPattern(text,pattern)
+    print(matchIndex)
+
+
+if __name__ == "__main__":
+    main()
