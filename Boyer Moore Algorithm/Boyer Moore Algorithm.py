7fb1652037305cafb4e2c3c1eaa386db0c30f138
Md. Saif Ahammod Khan <saif.ahammod@gmail.com>
2023-09-17 23:49:15 +0600

    BM algorithm Nothing is doneeee

---
+++ Boyer Moore Algorithm/Boyer Moore Algorithm.py
@@ -0,0 +1,15 @@
+def BoyerMoore(text, pattern):
+    print(text)
+    print(pattern)
+
+
+
+
+
+def main():
+    text = "World's simplest online text line randomizer for web developers and programmers."
+    pattern = "developers"
+    BoyerMoore(text, pattern)
+
+if __name__ == '__main__':
+    main()
